a08ae87b838f0703b54520152f05aa8708b0ebba
stephen <stephn2027@gmail.com>
2021-12-17 12:06:13 +0900

    fixingNextExport

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,46 +1,65 @@
-import React,{useState,useEffect, useRef} from 'react'
-import { FaShoppingCart } from 'react-icons/fa'
+import React, { useState, useEffect, useRef } from 'react';
+import { FaShoppingCart } from 'react-icons/fa';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavStyle from '../styles/Nav.module.css';
 import logo from '../public/images/gameShopLogo.png';
 import Image from 'next/image';
-import {Navbar,Container} from 'react-bootstrap';
+import { Navbar, Container } from 'react-bootstrap';
 export default function Nav() {
-   
-    const [total, setTotal] = useState(0);
-    const totalRef = useRef();
-    useEffect(() => {
-      if (window.Snipcart) {
-        setTotal(Snipcart.store.getState().cart.total);
-      }
-    },[]);
+  const [total, setTotal] = useState(0);
+  const totalRef = useRef();
+  useEffect(() => {
+    if (window.Snipcart) {
+      setTotal(Snipcart.store.getState().cart.total);
+    }
+  }, []);
 
-  
+  const myLoader = ({ src, width, quality }) => {
+    return `https://example.com/${src}?w=${width}&q=${quality || 75}`;
+  };
 
-    return (
-        <React.Fragment> 
-   
-    <Navbar expand="lg" variant="light" bg="light" fixed='top' style={{zIndex:1,backgroundColor:"#079de6",background: "radial-gradient(circle at 50% 50%,#c8d9e2, #e3ecf1)"}} >
-    <Container>
-      <Navbar.Brand href="#"><Image src={logo} alt="logo" width={200} height={60}/></Navbar.Brand>
-      <a className="snipcart-checkout snipcart-summary" href="#" style={{textDecoration: "none",color:"var(--orange)"}} >
-      <span className="snipcart-summary">visit cart &nbsp;   </span>
-              <FaShoppingCart />
-              
-              <strong className="sr-only">Cart</strong>
-              <span 
-              ref={totalRef}
-              
-              className="snipcart-total-price">{new Intl.NumberFormat("en-US", {
-                style: "currency",
-                currency: "USD",
-              }).format(total)}</span>
-            </a>
-    </Container>
-    </Navbar>
-   
-        
-            
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      <Navbar
+        expand="lg"
+        variant="light"
+        bg="light"
+        fixed="top"
+        style={{
+          zIndex: 1,
+          backgroundColor: '#079de6',
+          background: 'radial-gradient(circle at 50% 50%,#c8d9e2, #e3ecf1)',
+        }}
+      >
+        <Container>
+          <Navbar.Brand href="#">
+            <Image
+            //   loader={myLoader}
+              src={logo}
+              alt="gameshop logo"
+              width={200}
+              height={60}
+              priority
+            />
+          </Navbar.Brand>
+          <a
+            className="snipcart-checkout snipcart-summary"
+            href="#"
+            style={{ textDecoration: 'none', color: 'var(--orange)' }}
+          >
+            <span className="snipcart-summary">visit cart &nbsp; </span>
+            <FaShoppingCart />
+
+            <strong className="sr-only">Cart</strong>
+            <span ref={totalRef} className="snipcart-total-price">
+              {new Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: 'USD',
+              }).format(total)}
+            </span>
+          </a>
+        </Container>
+      </Navbar>
+    </React.Fragment>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
-import Image from 'next/image';
+
 import {gsap} from 'gsap';
 import client from '../apolloClient';
 import { gql } from '@apollo/client';
